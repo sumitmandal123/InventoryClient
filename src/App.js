9dba1bfa557be50cbe3f36ae01278800c3f6a324
sumitmandal123 <31107039+sumitmandal123@users.noreply.github.com>
2021-02-24 15:10:53 +0530

    https://github.com/idisrajesh/InventoryClient.git

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './component/Home/Home';
-import Router from './router'
+import Router from './router';
+import Login from './component/Login/Login';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
 
-      <Home>
-      </Home>
+
       <ToastContainer></ToastContainer>
-      <Router></Router>
+      <BrowserRouter>
+                <Switch>
+                    <Route exact path="/login" component={Login}>
+                    </Route>
+                    <Route path="/" component={Router}>
+                    </Route>
+                </Switch>
+      </BrowserRouter>
+      
+
     </div>
   );
 }
--- src/router.js
+++ src/router.js
@@ -4,28 +4,34 @@ import Inventory from './component/inventory/Inventory';
 import Product from './component/product/Product';
 import ProductAdd from './component//product/ProductAdd';
 import WareHouse from './component/warehouse/WareHouse';
+import Login from './component/Login/Login';
+import Home from './component/Home/Home';
 
 function Router(params) {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/Dashboard">
-                    <Dashboarad />
-                </Route>
-                <Route path="/Inventory">
-                    <Inventory />
-                </Route>
-                <Route path="/Product">
-                    <Product />
-                </Route>
-                <Route path="/Warehouse">
-                    <WareHouse></WareHouse>
-                </Route>
-                <Route path="/Add/Product">
-                    <ProductAdd></ProductAdd>
-                </Route>
-            </Switch>
-        </BrowserRouter>
+        <div>
+            <Home>
+            </Home>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/Dashboard">
+                        <Dashboarad />
+                    </Route>
+                    <Route path="/Inventory">
+                        <Inventory />
+                    </Route>
+                    <Route path="/Product">
+                        <Product />
+                    </Route>
+                    <Route path="/Warehouse">
+                        <WareHouse></WareHouse>
+                    </Route>
+                    <Route path="/Add/Product">
+                        <ProductAdd></ProductAdd>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
     );
 }
 
